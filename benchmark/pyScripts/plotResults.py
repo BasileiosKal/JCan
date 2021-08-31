8da9a22008be8620867c9335c04e6e2778547dbb
Vasilis Kalos <kalosbasileios@gmail.com>
2021-08-31 22:37:49 +0300

    Update gitignore

--- benchmark/pyScripts/plotResults.py
+++ benchmark/pyScripts/plotResults.py
@@ -59,37 +59,12 @@ SAVE_TO = "C:/Users/Vasilis/Desktop/Thesis/Code/BbsJSONsignatures/Fast/benchmark
 # Plot the results
 fig, ax = plt.subplots(2, 2, sharex=True, sharey=True, figsize=(FIG_X, FIG_Y))
 
-# fig_BN2, ax_BN2 = plt.subplots(figsize=(FIG_X, FIG_Y))
-# ax_BN2.plot(CLAIMS, np.multiply(np.array(JSONresults["BN_2"]), 1000), **JSON_LINE_CONFIG, label = "JSON")
-# ax_BN2.plot(CLAIMS, np.multiply(np.array(JSONLDresults["BN_2"]), 1000), **JSON_LD_LINE_CONFIG, label = "JSON LD")
-
-# ax_BN2.set(xlabel='Claims', ylabel='msec',
-#        title='Max Depth = 1. Blank Nodes = 2')
-# ax_BN2.grid()
-# plt.legend(prop = LEGEND_CONFIG)
-# plt.yticks(y_ticks_range)
-# plt.savefig(SAVE_TO+'/BN2.png')
-
-
 ax[0,0].plot(CLAIMS, np.multiply(np.array(JSONresults["BN_2"]), 1000), **JSON_LINE_CONFIG, label = "JSON")
 ax[0,0].plot(CLAIMS, np.multiply(np.array(JSONLDresults["BN_2"]), 1000), **JSON_LD_LINE_CONFIG, label = "JSON LD")
 
 ax[0,0].set(ylabel='msec',
        title='Max Depth = 2. Blank Nodes = 2')
 ax[0,0].grid()
-
-
-
-# fig_BN4, ax_BN4 = plt.subplots(figsize=(FIG_X, FIG_Y))
-# ax_BN4.plot(CLAIMS, np.multiply(np.array(JSONresults["BN_4"]), 1000), **JSON_LINE_CONFIG, label = "JSON")
-# ax_BN4.plot(CLAIMS, np.multiply(np.array(JSONLDresults["BN_4"]), 1000), **JSON_LD_LINE_CONFIG, label = "JSON LD")
-
-# ax_BN4.set(xlabel='Claims', ylabel='msec',
-#        title='Max Depth = 2. Blank Nodes = 4')
-# ax_BN4.grid()
-# plt.legend(prop = LEGEND_CONFIG)
-# plt.yticks(y_ticks_range)
-# plt.savefig(SAVE_TO+'/BN4.png')
 
 
 ax[0, 1].plot(CLAIMS, np.multiply(np.array(JSONresults["BN_4"]), 1000), **JSON_LINE_CONFIG, label = "JSON")
@@ -100,17 +75,6 @@ ax[0, 1].set( # xlabel='Claims', ylabel='msec',
 ax[0, 1].grid()
 
 
-# fig_BN8, ax_BN8 = plt.subplots(figsize=(FIG_X, FIG_Y))
-# ax_BN8.plot(CLAIMS, np.multiply(np.array(JSONresults["BN_8"]), 1000), **JSON_LINE_CONFIG, label = "JSON")
-# ax_BN8.plot(CLAIMS, np.multiply(np.array(JSONLDresults["BN_8"]), 1000), **JSON_LD_LINE_CONFIG, label = "JSON LD")
-
-# ax_BN8.set(xlabel='Claims', ylabel='msec',
-#        title='Max Depth = 3. Blank Nodes = 8')
-# ax_BN8.grid()
-# plt.legend(prop = LEGEND_CONFIG)
-# plt.yticks(y_ticks_range)
-# plt.savefig(SAVE_TO+'/BN8.png')
-
 ax[1, 0].plot(CLAIMS, np.multiply(np.array(JSONresults["BN_8"]), 1000), **JSON_LINE_CONFIG, label = "JSON")
 ax[1, 0].plot(CLAIMS, np.multiply(np.array(JSONLDresults["BN_8"]), 1000), **JSON_LD_LINE_CONFIG, label = "JSON LD")
 
@@ -118,17 +82,6 @@ ax[1, 0].set(xlabel='Claims', ylabel='msec',
        title='Max Depth = 8. Blank Nodes = 8')
 ax[1, 0].grid()
 
-
-# fig_BN16, ax_BN16 = plt.subplots(figsize=(FIG_X, FIG_Y))
-# ax_BN16.plot(CLAIMS, np.multiply(np.array(JSONresults["BN_16"]), 1000), **JSON_LINE_CONFIG, label = "JSON")
-# ax_BN16.plot(CLAIMS, np.multiply(np.array(JSONLDresults["BN_16"]), 1000), **JSON_LD_LINE_CONFIG, label = "JSON LD")
-
-# ax_BN16.set(xlabel='Claims', ylabel='msec',
-#        title='Max Depth = 3. Blank Nodes = 16')
-# ax_BN16.grid()
-# plt.legend(prop = LEGEND_CONFIG)
-# plt.yticks(y_ticks_range)
-# plt.savefig(SAVE_TO+'/BN16.png')
 
 ax[1, 1].plot(CLAIMS, np.multiply(np.array(JSONresults["BN_16"]), 1000), **JSON_LINE_CONFIG, label = "JSON")
 ax[1, 1].plot(CLAIMS, np.multiply(np.array(JSONLDresults["BN_16"]), 1000), **JSON_LD_LINE_CONFIG, label = "JSON LD")
